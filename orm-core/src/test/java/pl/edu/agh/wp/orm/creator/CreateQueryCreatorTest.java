cad720bdc971797ab978f47e82879b6d7fe38178
Piotrek <piotr06061995@gmail.com>
2017-02-10 20:39:09 +0100

    xd2

--- orm-core/src/test/java/pl/edu/agh/wp/orm/creator/CreateQueryCreatorTest.java
+++ orm-core/src/test/java/pl/edu/agh/wp/orm/creator/CreateQueryCreatorTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import pl.edu.agh.wp.orm.dto.DBTableObject;
 import pl.edu.agh.wp.orm.dto.queries.DBQuery;
+import pl.edu.agh.wp.orm.example.PhoneNumber;
 import pl.edu.agh.wp.orm.example.Pirson;
 import pl.edu.agh.wp.orm.example.Person;
 import pl.edu.agh.wp.orm.mapper.TableMapper;
@@ -25,9 +26,8 @@ public class CreateQueryCreatorTest {
     public void toSQLString() throws Exception {
         String expected = "CREATE TABLE Pirson (PirsonID INTEGER, CUDO_AGE INTEGER, Name VARCHAR(100), firstname VARCHAR(100), date DATE);";
         DBTableObject table = mapper.getTable(Pirson.class);
-        Pirson p = new Pirson();
         QueryCreator queryCreator = new CreateQueryCreator(table);
-        DBQuery query = queryCreator.createQuery(p);
+        DBQuery query = queryCreator.createQuery(null);
         Assert.assertEquals(expected, query.getSQLQuery());
     }
 
@@ -35,9 +35,19 @@ public class CreateQueryCreatorTest {
     public void toSQLStringPerson() throws Exception {
         String expected = "CREATE TABLE Person (per_id INTEGER, lastname VARCHAR(50), age INTEGER, birth_date DATE, name VARCHAR(50));";
         DBTableObject table = mapper.getTable(Person.class);
-        Person p = new Person();
         QueryCreator queryCreator = new CreateQueryCreator(table);
-        DBQuery query = queryCreator.createQuery(p);
+        DBQuery query = queryCreator.createQuery(null);
         Assert.assertEquals(expected, query.getSQLQuery());
+    }
+
+    @Test
+    public void toSQLStringPhone() throws Exception {
+
+        DBTableObject table = mapper.getTable(PhoneNumber.class);
+        QueryCreator queryCreator = new CreateQueryCreator(table);
+        DBQuery query = queryCreator.createQuery(null);
+//        Assert.assertEquals(expected, query.getSQLQuery());
+
+
     }
 }
