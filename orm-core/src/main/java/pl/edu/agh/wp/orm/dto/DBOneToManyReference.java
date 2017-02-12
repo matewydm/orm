267c6e446686988d0bed16acc7718962fb6b07e8
matewydm <mucha_303@outlook.com>
2017-02-12 18:13:21 +0100

    exception dla metod z kolumn

--- orm-core/src/main/java/pl/edu/agh/wp/orm/dto/DBOneToManyReference.java
+++ orm-core/src/main/java/pl/edu/agh/wp/orm/dto/DBOneToManyReference.java
@@ -1,11 +1,8 @@
 package pl.edu.agh.wp.orm.dto;
 
-import pl.ed.agh.wp.orm.annotations.enums.DBFetchType;
 import pl.edu.agh.wp.orm.exception.ORMException;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.Collection;
 
 public class DBOneToManyReference extends DBAbstractReference{
 
@@ -19,5 +16,60 @@ public class DBOneToManyReference extends DBAbstractReference{
         } catch (IllegalAccessException   e) {
             throw new ORMException("Cannot set Object ",e);
         }
+    }
+
+    @Override
+    public String getColumnName() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isUnique() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isNullable() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getMaxLength() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getScale() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getPrecision() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setUnique(boolean unique) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setNullable(boolean nullable) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setMaxLength(int maxLength) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setScale(int scale) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setPrecision(int precision) {
+        throw new UnsupportedOperationException();
     }
 }
